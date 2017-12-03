b159f2a2b1f6bf6778847653ccc751b3f438d0b0
Stewart Gibson <sg4654@nyu.edu>
2017-12-03 18:06:28 -0500

    Initial content push

---
+++ Scripts/Distributions.R
@@ -0,0 +1,107 @@
+############################################################## 
+##  ASA NFL Site Content                                    ##
+##  Player Fantasy point, salary, and FP/$1K distributions  ##
+##  Stewart Gibson                                          ##
+##  7/8/17                                                  ##
+##############################################################
+
+## Load packages
+require(ggplot2)
+
+## Load data
+load("NFL/data/clean_data_2016_2017.RData")
+
+# ## Create dataframe of each players season point totals, only include players who scored above
+# ## positional cutoff levels.
+# season.totals <- cbind(unique(Fantasy.2016[,c("First.Last","Team")]), 
+#                        data.frame(pos = NA, DK.points.total = NA))
+# 
+# for  (i in 1:nrow(season.totals)) {
+#   season.totals$pos[i] <- as.character(Fantasy.2016$Pos[which(
+#     Fantasy.2016$First.Last == season.totals$First.Last[i] &
+#       Fantasy.2016$Team == season.totals$Team[i])][1])
+#   season.totals$DK.points.total[i] <- sum(Fantasy.2016$DK.points[which(
+#     Fantasy.2016$First.Last == season.totals$First.Last[i] &
+#       Fantasy.2016$Team == season.totals$Team[i])])
+# }
+# 
+# QB.cutoff <- 20
+# RB.cutoff <- 45
+# WR_TE.cutoff <- 30
+# 
+# ## Set unique players list (only considering players who scored above their positional cutoff)
+# uniq.players <- season.totals[which(
+#   (season.totals$pos == 'QB' & season.totals$DK.points.total >= QB.cutoff)|
+#     (season.totals$pos == 'RB' & season.totals$DK.points.total >= RB.cutoff)|
+#     (season.totals$pos == 'WR' & season.totals$DK.points.total >= WR_TE.cutoff) |
+#     (season.totals$pos == 'TE' & season.totals$DK.points.total >= WR_TE.cutoff)), c(1,2,3)]
+
+uniq.players <- unique(Fantasy.2016_2017$First.Last)
+for (i in 1:length(uniq.players)) {
+  dat <- Fantasy.2016_2017[which(Fantasy.2016_2017$First.Last == uniq.players[i]),]
+  
+  # set color scheme
+  if (dat$Team[length(dat$Team)] == "ari") {col.set = c("darkred","white")
+  } else if (dat$Team[length(dat$Team)] == "atl") {col.set = c("black","red")
+  } else if (dat$Team[length(dat$Team)] == "bal") {col.set = c("darkorchid4","goldenrod")
+  } else if (dat$Team[length(dat$Team)] == "buf") {col.set = c("mediumblue","white")
+  } else if (dat$Team[length(dat$Team)] == "car") {col.set = c("black","deepskyblue")
+  } else if (dat$Team[length(dat$Team)] == "chi") {col.set = c("darkblue","orange")
+  } else if (dat$Team[length(dat$Team)] == "cin") {col.set = c("darkorange","black")
+  } else if (dat$Team[length(dat$Team)] == "cle") {col.set = c("darkorange2","chocolate4")
+  } else if (dat$Team[length(dat$Team)] == "dal") {col.set = c("gray60","midnightblue")
+  } else if (dat$Team[length(dat$Team)] == "den") {col.set = c("navy","darkorange2")
+  } else if (dat$Team[length(dat$Team)] == "det") {col.set = c("dodgerblue","grey")
+  } else if (dat$Team[length(dat$Team)] == "gnb") {col.set = c("green4","yellow")
+  } else if (dat$Team[length(dat$Team)] == "hou") {col.set = c("red3","royalblue4")
+  } else if (dat$Team[length(dat$Team)] == "ind") {col.set = c("white","blue3")
+  } else if (dat$Team[length(dat$Team)] == "jac") {col.set = c("turquoise","khaki")
+  } else if (dat$Team[length(dat$Team)] == "kan") {col.set = c("red","gold")
+  } else if (dat$Team[length(dat$Team)] == "lac") {col.set = c("deepskyblue","yellow")
+  } else if (dat$Team[length(dat$Team)] == "lar") {col.set = c("navy","white")
+  } else if (dat$Team[length(dat$Team)] == "mia") {col.set = c("turquoise","orange")
+  } else if (dat$Team[length(dat$Team)] == "min") {col.set = c("purple","gold")
+  } else if (dat$Team[length(dat$Team)] == "nor") {col.set = c("lightgoldenrod","black")
+  } else if (dat$Team[length(dat$Team)] == "nwe") {col.set = c("royalblue4","red4")
+  } else if (dat$Team[length(dat$Team)] == "nyg") {col.set = c("blue","red")
+  } else if (dat$Team[length(dat$Team)] == "nyj") {col.set = c("darkgreen","white")
+  } else if (dat$Team[length(dat$Team)] == "oak") {col.set = c("grey","black")
+  } else if (dat$Team[length(dat$Team)] == "phi") {col.set = c("seagreen4","black")
+  } else if (dat$Team[length(dat$Team)] == "pit") {col.set = c("black", "yellow")
+  } else if (dat$Team[length(dat$Team)] == "sea") {col.set = c("navy","chartreuse")
+  } else if (dat$Team[length(dat$Team)] == "sfo") {col.set = c("red","darkgoldenrod")
+  } else if (dat$Team[length(dat$Team)] == "tam") {col.set = c("red3","burlywood4")
+  } else if (dat$Team[length(dat$Team)] == "ten") {col.set = c("steelblue1","steelblue4")
+  } else {col.set = c("orangered4","gold")}
+  
+  # Plot distributions
+  ggplot(data = dat) +
+    stat_density(aes(DK.points), adjust = 0.5, color = col.set[2], fill = col.set[1], size = 1) +
+    xlab("DK Fantasy Points") +
+    scale_x_continuous(limits = c(-10,60)) +
+    scale_y_continuous(limits = c(0, .2)) +
+    ggtitle(paste(dat$First.Last[1], "DK Points Distribution", sep = " ")) +
+    theme(plot.title = element_text(hjust = 0.5))
+  ggsave(paste('NFL/Visualizations/Distributions/By Position/DK Points/',
+               dat$Pos[1],"/", dat$First.Last[1],'.png', sep = ''))
+
+  ggplot(data = dat[which(dat$DK.salary > 0),]) +
+    stat_density(aes(DK.salary), adjust = 0.5, color = col.set[2], fill = col.set[1], size = 1) +
+    xlab("DK Salary") +
+    scale_x_continuous(limits = c(1000, 12000)) +
+    scale_y_continuous(limits = c(0, 0.0015)) +
+    ggtitle(paste(dat$First.Last[1], "DK Salary Distribution", sep = " ")) +
+    theme(plot.title = element_text(hjust = 0.5))
+  ggsave(paste('NFL/Visualizations/Distributions/By Position/Salary/',
+               dat$Pos[1],"/", dat$First.Last[1],'.png', sep = ''))
+
+  ggplot(data = dat[which(dat$DK.salary > 0),]) +
+    stat_density(aes(DK.points/(DK.salary/1000)), adjust = 0.5, color = col.set[2], fill = col.set[1], size = 1) +
+    xlab("DK Points/$1K") +
+    scale_x_continuous(limits = c(-1, 12)) +
+    scale_y_continuous(limits = c(0, .75)) +
+    ggtitle(paste(dat$First.Last[1], "DK Fantasy Points/$1K", sep = " ")) +
+    theme(plot.title = element_text(hjust = 0.5))
+  ggsave(paste('NFL/Visualizations/Distributions/By Position/DKP per $1K/',
+               dat$Pos[1],"/", dat$First.Last[1],'.png', sep = ''))
+}
---
+++ Scripts/Top Positional Probability.R
@@ -0,0 +1,139 @@
+################################## 
+##  ASA NFL Site Content        ##
+##  Top Positional Probability  ##
+##  Stewart Gibson              ##
+##  8/31/17                     ##
+##################################
+
+### Import data, separate into 2016 and 2015 datasets
+setwd("~/Documents/ASA/ASA Site Content")
+load("NFL/data/clean_data_2016_2017.RData")
+
+### Create list of unique offensive players
+uniq.players <- unique(Fantasy.2016_2017[which(Fantasy.2016_2017$Pos != "Def"),c("First.Last","Team")])
+                                                              
+### Read in projected players .csv
+DraftKings.players <- read.csv("NFL/data/DraftKings.Lineup.csv")
+# Keep only players who aren't injured
+DraftKings.players <- DraftKings.players[which(is.na(DraftKings.players$Injury)),]
+
+### Create large for loop that runs n simulations of drawing a fantasy total from every
+### player's fantasy distribution function.  Store in a data frame for each position.
+iter <- 10000
+
+### Quarterback (QB)
+uniq.QB <- unique(DraftKings.players$Name[which(DraftKings.players$Pos == "QB")])
+samp.QB <- matrix(NA, nrow = iter, ncol = length(uniq.QB))
+colnames(samp.QB) <- uniq.QB
+for (i in c(1:length(uniq.QB))) {
+  dat <- Fantasy.2016_2017[which(Fantasy.2016_2017$First.Last == as.character(uniq.QB[i])),]
+  if (nrow(dat) > 1) {
+    player.dens <- density(dat$DK.points)
+    
+    samp.QB[,i] <- sample(player.dens$x, iter, replace = T, prob = player.dens$y)
+  } else (samp.QB[,i] <- 0)
+}
+
+Top.QB <- matrix(NA, nrow = iter, ncol = 1)
+for (i in c(1:iter)) {
+  Top.QB[i,1] <- which.max(samp.QB[i,])
+}
+
+Top.QB.probs <- data.frame(Player = uniq.QB, Matchup = NA, Salary = NA, Prob = NA)
+for (i in c(1:nrow(Top.QB.probs))) {
+  Top.QB.probs$Salary[i] <- DraftKings.players$Salary[which(DraftKings.players$Name ==
+                                                             Top.QB.probs$Player[i])]
+  Top.QB.probs$Matchup[i] <- as.character(DraftKings.players$GameInfo[which(DraftKings.players$Name ==
+                                                                 Top.QB.probs$Player[i])])
+  Top.QB.probs$Prob[i] <- length(Top.QB[which(Top.QB[,1] == i)])/iter
+}
+Top.QB.probs <- cbind(data.frame(Rank = 1:nrow(Top.QB.probs)), Top.QB.probs[order(Top.QB.probs$Prob,decreasing = T),])
+write.csv(Top.QB.probs, file = "NFL/TPPs/QB.csv", row.names = F)
+
+### Running Back (RB)
+uniq.RB <- unique(DraftKings.players$Name[which(DraftKings.players$Pos == "RB")])
+samp.RB <- matrix(NA, nrow = iter, ncol = length(uniq.RB))
+colnames(samp.RB) <- uniq.RB
+for (i in c(1:length(uniq.RB))) {
+  dat <- Fantasy.2016_2017[which(Fantasy.2016_2017$First.Last == as.character(uniq.RB[i])),]
+  if (nrow(dat) > 1) {
+    player.dens <- density(dat$DK.points)
+    
+    samp.RB[,i] <- sample(player.dens$x, iter, replace = T, prob = player.dens$y)
+  } else (samp.RB[,i] <- 0)
+}
+
+Top.RB <- matrix(NA, nrow = iter, ncol = 1)
+for (i in c(1:iter)) {
+  Top.RB[i,1] <- which.max(samp.RB[i,])
+}
+
+Top.RB.probs <- data.frame(Player = uniq.RB, Matchup = NA, Salary = NA, Prob = NA)
+for (i in c(1:nrow(Top.RB.probs))) {
+  Top.RB.probs$Salary[i] <- DraftKings.players$Salary[which(DraftKings.players$Name ==
+                                                              Top.RB.probs$Player[i])]
+  Top.RB.probs$Matchup[i] <- as.character(DraftKings.players$GameInfo[which(DraftKings.players$Name ==
+                                                                              Top.RB.probs$Player[i])])
+  Top.RB.probs$Prob[i] <- length(Top.RB[which(Top.RB[,1] == i)])/iter
+}
+Top.RB.probs <- cbind(data.frame(Rank = 1:nrow(Top.RB.probs)), Top.RB.probs[order(Top.RB.probs$Prob,decreasing = T),])
+write.csv(Top.RB.probs, file = "NFL/TPPs/RB.csv", row.names = F)
+
+### Wide Receiver (WR)
+uniq.WR <- unique(DraftKings.players$Name[which(DraftKings.players$Pos == "WR")])
+samp.WR <- matrix(NA, nrow = iter, ncol = length(uniq.WR))
+colnames(samp.WR) <- uniq.WR
+for (i in c(1:length(uniq.WR))) {
+  dat <- Fantasy.2016_2017[which(Fantasy.2016_2017$First.Last == as.character(uniq.WR[i])),]
+  if (nrow(dat) > 1) {
+    player.dens <- density(dat$DK.points)
+    
+    samp.WR[,i] <- sample(player.dens$x, iter, replace = T, prob = player.dens$y)
+  } else (samp.WR[,i] <- 0)
+}
+
+Top.WR <- matrix(NA, nrow = iter, ncol = 1)
+for (i in c(1:iter)) {
+  Top.WR[i,1] <- which.max(samp.WR[i,])
+}
+
+Top.WR.probs <- data.frame(Player = uniq.WR, Matchup = NA, Salary = NA, Prob = NA)
+for (i in c(1:nrow(Top.WR.probs))) {
+  Top.WR.probs$Salary[i] <- DraftKings.players$Salary[which(DraftKings.players$Name ==
+                                                              Top.WR.probs$Player[i])]
+  Top.WR.probs$Matchup[i] <- as.character(DraftKings.players$GameInfo[which(DraftKings.players$Name ==
+                                                                              Top.WR.probs$Player[i])])
+  Top.WR.probs$Prob[i] <- length(Top.WR[which(Top.WR[,1] == i)])/iter
+}
+Top.WR.probs <- cbind(data.frame(Rank = 1:nrow(Top.WR.probs)), Top.WR.probs[order(Top.WR.probs$Prob,decreasing = T),])
+write.csv(Top.WR.probs, file = "NFL/TPPs/WR.csv", row.names = F)
+
+### Tight End (TE)
+uniq.TE <- unique(DraftKings.players$Name[which(DraftKings.players$Pos == "TE")])
+samp.TE <- matrix(NA, nrow = iter, ncol = length(uniq.TE))
+colnames(samp.TE) <- uniq.TE
+for (i in c(1:length(uniq.TE))) {
+  dat <- Fantasy.2016_2017[which(Fantasy.2016_2017$First.Last == as.character(uniq.TE[i]) &
+                              Fantasy.2016_2017$Pos == "TE"),]
+  if (nrow(dat) > 1) {
+    player.dens <- density(dat$DK.points)
+    
+    samp.TE[,i] <- sample(player.dens$x, iter, replace = T, prob = player.dens$y)
+  } else (samp.TE[,i] <- 0)
+}
+
+Top.TE <- matrix(NA, nrow = iter, ncol = 1)
+for (i in c(1:iter)) {
+  Top.TE[i,1] <- which.max(samp.TE[i,])
+}
+
+Top.TE.probs <- data.frame(Player = uniq.TE, Matchup = NA, Salary = NA, Prob = NA)
+for (i in c(1:nrow(Top.TE.probs))) {
+  Top.TE.probs$Salary[i] <- DraftKings.players$Salary[which(DraftKings.players$Name ==
+                                                              Top.TE.probs$Player[i])]
+  Top.TE.probs$Matchup[i] <- as.character(DraftKings.players$GameInfo[which(DraftKings.players$Name ==
+                                                                              Top.TE.probs$Player[i])])
+  Top.TE.probs$Prob[i] <- length(Top.TE[which(Top.TE[,1] == i)])/iter
+}
+Top.TE.probs <- cbind(data.frame(Rank = 1:nrow(Top.TE.probs)), Top.TE.probs[order(Top.TE.probs$Prob,decreasing = T),])
+write.csv(Top.TE.probs, file = "NFL/TPPs/TE.csv", row.names = F)
