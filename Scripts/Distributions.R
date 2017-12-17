38f1a2eb8b492fae2301e25416355e19bcdb5bf7
Stewart Gibson <sg4654@nyu.edu>
2017-12-17 15:41:03 -0500

    Script update

--- Scripts/Distributions.R
+++ Scripts/Distributions.R
@@ -102,6 +102,6 @@ for (i in 1:length(uniq.players)) {
     scale_y_continuous(limits = c(0, .75)) +
     ggtitle(paste(dat$First.Last[1], "DK Fantasy Points/$1K", sep = " ")) +
     theme(plot.title = element_text(hjust = 0.5))
-  ggsave(paste('Visualizations/Distributions/By Position/DKP per $1K/',
+  ggsave(paste('Visualizations/Distributions/By Position/DKP per 1K/',
                dat$Pos[1],"/", dat$First.Last[1],'.png', sep = ''))
 }
